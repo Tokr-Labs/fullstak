c728d1b0cad5666c53d28e5a51bd655a84889c87
T.J. Kyner <tjkyner@gmail.com>
2022-04-19 13:41:29 -0400

    logon links to home

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Grid, useTheme} from "@nextui-org/react";
 import {WalletMultiButton} from "@solana/wallet-adapter-react-ui";
+import {Link} from "react-router-dom";
 
 export const Navbar = () => {
 
@@ -9,10 +10,12 @@ export const Navbar = () => {
     return (
         <Grid.Container gap={1}>
             <Grid xs={6} alignItems={"center"}>
-                <img src={require("src/assets/tokr_" + (theme.isDark ? "dark" : "light") + ".png")}
-                     height={"auto"}
-                     width={"150px"}
-                     alt={"tokr logo"}/>
+                <Link to="/">
+                    <img src={require("src/assets/tokr_" + (theme.isDark ? "dark" : "light") + ".png")}
+                         height={"auto"}
+                         width={"150px"}
+                         alt={"tokr logo"}/>
+                </Link>
             </Grid>
             <Grid xs={6} justify={"flex-end"} alignItems={"center"}>
                 <WalletMultiButton/>
