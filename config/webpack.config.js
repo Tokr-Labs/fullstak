046794f4b423adb0d28f12258600767154db707e
T.J. Kyner <tjkyner@gmail.com>
2022-04-29 17:05:11 -0400

    undo config change

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -344,7 +344,7 @@ module.exports = function (webpackEnv) {
                 "https": false,
                 "stream": false,
                 "crypto": false,
-                // "os": false,
+                "os": false,
                 "crypto-browserify": require.resolve('crypto-browserify'), // if you want to use this module also don't forget npm i crypto-browserify
             }
 
