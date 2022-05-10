2b694fa6b059ab75d3f2e67ea503416a5c4b1630
T.J. Kyner <tjkyner@gmail.com>
2022-05-10 16:24:27 -0400

    equity market tables

--- src/components/EquityMarkets.tsx
+++ src/components/EquityMarkets.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Button, Card, Grid, Text, Table, theme} from "@nextui-org/react";
+import {Button, Card, Grid, Text, Table, theme, Progress, User} from "@nextui-org/react";
 import {Link} from "react-router-dom";
 
 export const EquityMarkets = () => {
@@ -14,39 +14,47 @@ export const EquityMarkets = () => {
                 <Card>
 
                     <Card.Header style={{padding: "20px 0 0 20px"}}>
-                        <Text h3>Raising</Text>
+                        <Text h3 css={{letterSpacing: "2px"}}>OPEN FUNDS</Text>
                     </Card.Header>
 
                     <Card.Body>
                         <Table shadow={false} sticked headerLined>
                             <Table.Header>
-                                <Table.Column>Name</Table.Column>
-                                <Table.Column>Token</Table.Column>
-                                <Table.Column align={"end"} css={{paddingRight: theme.space["5"].computedValue}}>Total Raised</Table.Column>
-                                <Table.Column align={"end"} css={{paddingRight: theme.space["5"].computedValue}}>Max Raise</Table.Column>
+                                <Table.Column>Fund</Table.Column>
                                 <Table.Column align={"end"} css={{paddingRight: theme.space["5"].computedValue}}>Target IRR</Table.Column>
                                 <Table.Column align={"end"} css={{paddingRight: theme.space["5"].computedValue}}>Target TVPI</Table.Column>
                                 <Table.Column align={"end"} css={{paddingRight: theme.space["5"].computedValue}}>Target DPI</Table.Column>
-                                <Table.Column align={"center"}>Strategy</Table.Column>
-                                <Table.Column align={"end"} css={{paddingRight: theme.space["5"].computedValue}}>Target Close</Table.Column>
+                                <Table.Column align={"end"} css={{paddingRight: theme.space["5"].computedValue}}>Strategy</Table.Column>
+                                <Table.Column align={"end"} css={{paddingRight: theme.space["5"].computedValue}}>Max Raise</Table.Column>
+                                <Table.Column align={"center"}>Progress</Table.Column>
                                 <Table.Column children=""/>
                             </Table.Header>
                             <Table.Body>
                                 <Table.Row>
-                                    <Table.Cell>{data.name}</Table.Cell>
-                                    {/*TODO - include ticker of token and link to explorer page here*/}
-                                    <Table.Cell>{data.token.ticker}</Table.Cell>
-                                    {/*TODO - since LP tokens are issued 1:1, use the outstanding supply here*/}
-                                    <Table.Cell css={{textAlign: "end"}}>587,250 USDC</Table.Cell>
-                                    <Table.Cell css={{textAlign: "end"}}>{data.details.max_raise}</Table.Cell>
+                                    <Table.Cell>
+                                        <User size={"sm"} name={data.name}/>
+                                    </Table.Cell>
                                     <Table.Cell css={{textAlign: "end"}}>{data.details.target_returns.irr}</Table.Cell>
-                                    <Table.Cell css={{textAlign: "end"}}>1.75x</Table.Cell>
-                                    <Table.Cell css={{textAlign: "end"}}>1.75x</Table.Cell>
-                                    <Table.Cell css={{textAlign: "center"}}>Value-Add Multifamily</Table.Cell>
-                                    <Table.Cell css={{textAlign: "end"}}>{data.details.raise_close}</Table.Cell>
-                                    <Table.Cell css={{textAlign: "end", float: "right"}}>
+                                    <Table.Cell css={{textAlign: "end"}}>4.30x</Table.Cell>
+                                    <Table.Cell css={{textAlign: "end"}}>2.10x</Table.Cell>
+                                    <Table.Cell css={{textAlign: "end"}}>Value-Add</Table.Cell>
+                                    <Table.Cell css={{textAlign: "end"}}>{data.details.max_raise}</Table.Cell>
+                                    {/*TODO - since LP tokens are issued 1:1, use the outstanding supply here*/}
+                                    <Table.Cell css={{minWidth: "200px", padding: "15px 20px 5px 20px"}}>
+                                        <Progress size={"sm"} value={50}/>
+                                        <Text size={12} color={"gray"}>$5,000,000 Raised</Text>
+                                    </Table.Cell>
+                                    <Table.Cell css={{textAlign: "end", float: "right", margin: "5px 0"}}>
                                         <Link to={"/markets/equity/pool-details"}>
-                                            <Button size={"xs"} style={{margin: 0}}>View Details</Button>
+                                            <Button
+                                                ghost
+                                                color={"primary"}
+                                                size={"xs"}
+                                                borderWeight={"light"}
+                                                style={{margin: 0, fontWeight: "bold", borderRadius: 0}}
+                                            >
+                                                INVEST
+                                            </Button>
                                         </Link>
                                     </Table.Cell>
                                 </Table.Row>
@@ -65,31 +73,63 @@ export const EquityMarkets = () => {
                 <Card>
 
                     <Card.Header style={{padding: "20px 0 0 20px"}}>
-                        <Text h3>Active</Text>
+                        <Text h3 css={{letterSpacing: "2px"}}>ACTIVE FUNDS</Text>
                     </Card.Header>
 
                     <Card.Body>
                         <Table shadow={false} sticked headerLined>
                             <Table.Header>
-                                <Table.Column>Name</Table.Column>
-                                <Table.Column>Token</Table.Column>
+                                <Table.Column>Fund</Table.Column>
                                 <Table.Column align={"end"} css={{paddingRight: theme.space["5"].computedValue}}>Paid-in Capital</Table.Column>
                                 <Table.Column align={"end"} css={{paddingRight: theme.space["5"].computedValue}}>Carrying Value</Table.Column>
                                 <Table.Column align={"end"} css={{paddingRight: theme.space["5"].computedValue}}>TVPI</Table.Column>
                                 <Table.Column align={"end"} css={{paddingRight: theme.space["5"].computedValue}}>DPI</Table.Column>
                                 <Table.Column align={"end"} css={{paddingRight: theme.space["5"].computedValue}}>Net IRR</Table.Column>
-                                <Table.Column align={"end"}>Vintage Year</Table.Column>
+                                <Table.Column align={"end"}>Fund Vintage</Table.Column>
+                                <Table.Column children={""}/>
                             </Table.Header>
                             <Table.Body>
                                 <Table.Row>
-                                    <Table.Cell>Miami DAO</Table.Cell>
-                                    <Table.Cell>MIA</Table.Cell>
-                                    <Table.Cell css={{textAlign: "end"}}>10,000,000 USDC</Table.Cell>
-                                    <Table.Cell css={{textAlign: "end"}}>15,000,000 USDC</Table.Cell>
-                                    <Table.Cell css={{textAlign: "end"}}>4.75x</Table.Cell>
+                                    <Table.Cell>
+                                        <User size={"sm"} name={"Participant Crypto"}/>
+                                    </Table.Cell>
+                                    <Table.Cell css={{textAlign: "end"}}>10M USDC</Table.Cell>
+                                    <Table.Cell css={{textAlign: "end"}}>15M USDC</Table.Cell>
+                                    <Table.Cell css={{textAlign: "end"}}>4.50x</Table.Cell>
                                     <Table.Cell css={{textAlign: "end"}}>1.75x</Table.Cell>
                                     <Table.Cell css={{textAlign: "end"}}>27%</Table.Cell>
-                                    <Table.Cell css={{textAlign: "end"}}>2022</Table.Cell>
+                                    <Table.Cell css={{textAlign: "end"}}>May 2022</Table.Cell>
+                                    <Table.Cell css={{textAlign: "end", float: "right", margin: "5px 0"}}>
+                                        <Button
+                                            light
+                                            size={"xs"}
+                                            borderWeight={"light"}
+                                            style={{margin: 0, fontWeight: "bold"}}
+                                        >
+                                            DETAILS
+                                        </Button>
+                                    </Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell>
+                                        <User size={"sm"} name={"Full Send"}/>
+                                    </Table.Cell>
+                                    <Table.Cell css={{textAlign: "end"}}>1B USDC</Table.Cell>
+                                    <Table.Cell css={{textAlign: "end"}}>1.42B USDC</Table.Cell>
+                                    <Table.Cell css={{textAlign: "end"}}>3.70x</Table.Cell>
+                                    <Table.Cell css={{textAlign: "end"}}>1.75x</Table.Cell>
+                                    <Table.Cell css={{textAlign: "end"}}>24%</Table.Cell>
+                                    <Table.Cell css={{textAlign: "end"}}>June 2022</Table.Cell>
+                                    <Table.Cell css={{textAlign: "end", float: "right", margin: "5px 0"}}>
+                                        <Button
+                                            light
+                                            size={"xs"}
+                                            borderWeight={"light"}
+                                            style={{margin: 0, fontWeight: "bold"}}
+                                        >
+                                            DETAILS
+                                        </Button>
+                                    </Table.Cell>
                                 </Table.Row>
                             </Table.Body>
                         </Table>
