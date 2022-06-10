561ae4a5634bb4b70b305d6768ebe4f27be9590e
BDANG <heybdang@gmail.com>
2022-06-10 14:48:55 -0400

    fund term as selectable presets

--- src/components/create-fund-views/FundConfig.tsx
+++ src/components/create-fund-views/FundConfig.tsx
@@ -1,13 +1,17 @@
-import { Grid, Input, Dropdown } from '@nextui-org/react';
+import { Grid, Input, Dropdown, Button, Text, theme } from '@nextui-org/react';
 import React from 'react'
 
 const FundConfig = (props) => {
     const validAssetClasses = ["A", "B", "C", "D"];
+    const [showCustomFundTerm, setShowCustomFundTerm] = React.useState<boolean>(false);
 
-    const fundTermYears = Array(30).fill(0).map((_, i) => i + 1);
+    const onClickFundTerm = (value: number | undefined) => {
+        props.setFundTerm(value);
+        setShowCustomFundTerm(false);
+    }
 
     // styling for dropdown buttons
-    const dropDownButtonStyle: any = {borderRadius: 19, textTransform: "capitalize"};
+    const dropDownButtonStyle: object = {borderRadius: 19, textTransform: "capitalize"};
     return (
         <>
         <Grid.Container gap={2}>                        
@@ -65,9 +69,34 @@ const FundConfig = (props) => {
             <Grid xs={3} justify='center'>
                 <Input label="Investment Strategy" value={props.strategy} onChange={event => props.setStrategy(event.target.value)}/>
             </Grid>
-            <Grid xs={3} justify='center'>
-                <Input label="Fund Term" type="number" value={props.fundTerm} onChange={event => props.setFundTerm(event.target.value)}/>
-            </Grid>
+            <Grid.Container xs={12} justify='center'>
+                <Grid xs={12} justify='center'>
+                    <Text>Fund Term (Years):</Text>
+                </Grid>
+                <Grid xs={12} justify='center'>
+                <Button.Group size="sm" color="primary" bordered > 
+                {/* style={{backgroundColor: "#F6DCFF"}} */}
+                    <Button ghost onClick={() => onClickFundTerm(1)}>One</Button>
+                    <Button ghost onClick={() => onClickFundTerm(2)}>Two</Button>
+                    <Button ghost onClick={() => onClickFundTerm(3)}>Three</Button>
+                    <Button ghost onClick={() => onClickFundTerm(4)}>Four</Button>
+                    <Button ghost onClick={() => onClickFundTerm(5)}>Five</Button>
+                    {/* Initially show the "Custom" button, but on-click change it to an input */}
+                    {!showCustomFundTerm ? (
+                        <Button onClick={() => {props.setFundTerm(undefined); setShowCustomFundTerm(true)}}>Custom</Button>
+                    ) : (
+                        <Input
+                            clearable
+                            size='sm'
+                            width='8ch'
+                            type="number"
+                            onClearClick={() => {onClickFundTerm(undefined)}}
+                            value={props.fundTerm} onChange={event => props.setFundTerm(event.target.value)}
+                        />
+                    )}
+                </Button.Group>
+                </Grid>
+            </Grid.Container>
 
             {/* Comically impractical -- wen slider??
             <Dropdown>
